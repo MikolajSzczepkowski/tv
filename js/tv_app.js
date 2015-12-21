379cd7433e2e0cd4569e00f267f20a621bbd5b8f
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-12-21 10:12:14 +0100

    hover correction

--- js/tv_app.js
+++ js/tv_app.js
@@ -6,12 +6,12 @@ $(function (){
 		thisContainerHover;
 	$(".stream-container").on("mouseenter", function(){
 		parentWidth = $(this).width(),
-		parentHeight = ($(this).height()+1),
+		parentHeight = ($(this).height()),
 		thisStreamContainer = $(this);
 		$(this).prepend("<div class='stream-container-hover'></div>");
 		$(".stream-container-hover").css({"width":parentWidth, "height": parentHeight});
 		$(".stream-container-hover").append("<button class='text-uppercase'>watch</button>");
-		$(".stream-container-hover").append("<div class='bottom-info text-center'>PSC-Paul<span><img src='images/user-small.png' alt='users'>27</span></div>'");
+		$(".stream-container-hover").append("<div class='bottom-info text-center'>PSC-Paul<span><img src='images/user-small.png' alt='users'>27</span></div>");
 		thisContainerHover = $(this).find(".stream-container-hover");
 		offset = thisStreamContainer.offset();
 		thisContainerHover.offset({

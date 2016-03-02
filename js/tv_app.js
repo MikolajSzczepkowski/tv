92c85da1c0ba23ece2541a75415c34705de75fe2
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2016-03-02 13:19:20 +0100

    request modal

--- js/tv_app.js
+++ js/tv_app.js
@@ -5,7 +5,6 @@ $(function (){
 		thisStreamContainer,
 		thisContainerHover,
 		offset,
-		streamContainerHeight = $("#streamContainer").height(),
 		comment = $(".comments-container li"),
 		listWidth;
 
@@ -29,25 +28,6 @@ $(function (){
 		}
 	});
 
-	$("li .stream-container-inner").on("mouseenter", function(){
-		parentWidth = $(this).width(),
-		parentHeight = ($(this).height()+1),
-		thisStreamContainer = $(this);
-		$(this).prepend("<div class='stream-container-hover'></div>");
-		$(".stream-container-hover").css({"width":parentWidth, "height": parentHeight});
-		$(".stream-container-hover").append("<span><img src='images/love-red.png' alt='like'>273</span>");
-		$(".stream-container-hover").append("<span><img src='images/user-small.png' alt='users'>123</span>");
-		$(".stream-container-hover").append("<button class='text-uppercase'>watch</button>");
-		thisContainerHover = $(this).find(".stream-container-hover");
-		offset = thisStreamContainer.offset();
-		thisContainerHover.offset({
-			top: offset.top,
-			left: offset.left
-		});
-	});
-	$("li .stream-container-inner").on("mouseleave", function(){
-		$(".stream-container-hover").remove();
-	});
 	$("ul").scroll(function(){
 		offset = thisStreamContainer.offset();
 		thisContainerHover.offset({
